7d33c8e0b74dec0257aa765431789cc8b1a23d2c
MSCosamaShallal <osamafalah85@yahoo.com>
2022-12-16 17:16:25 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,4 +10,37 @@ const person = {
   hobbies: ["sport", "movies", "movies"],
 };
 
-console.log(person.firstname, person.lastname);
+console.log(
+  person.firstname,
+  person.lastname,
+  person.address.city,
+  person.hobbies
+);
+
+const { firstname, lastname } = person;
+
+console.log(firstname);
+
+const todos = [
+  {
+    id: 1,
+    task: "take out trash",
+    isCompleted: true,
+  },
+  {
+    id: 2,
+    task: "Meeting  with boss",
+    isCompleted: false,
+  },
+  {
+    id: 3,
+    task: "dentist appt",
+    isCompleted: true,
+  },
+];
+const todoJson = JSON.stringify(todos);
+console.log(todoJson);
+
+for (let i = 0; i <= todos.length - 1; i++) {
+  console.log(todos[i].task);
+}
