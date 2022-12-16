c470d5d738f7061362fe765cd204ab9602a6ce1f
MSCosamaShallal <osamafalah85@yahoo.com>
2022-12-16 16:50:52 +0300

    fhfh

---
+++ main.js
@@ -0,0 +1,13 @@
+const person = {
+  firstname: "ali",
+  lastname: "mohammed",
+  age: 30,
+  address: {
+    streat: "50 main st",
+    city: "al-admaya",
+    state: "bg",
+  },
+  hobbies: ["sport", "movies", "movies"],
+};
+
+console.log(person.firstname, person.lastname);
